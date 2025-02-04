24f21ea5266ad0242eff7aee654554d1936c89d4
Veronica Ludemann <veronicaludemann67@gmail.com>
2025-02-04 17:05:24 -0300

    cambios en hooks 4

--- cypress/e2e/A - seccion 6/hooks_4.cy.js
+++ cypress/e2e/A - seccion 6/hooks_4.cy.js
@@ -1,10 +1,6 @@
 /// <reference types="cypress" />
 
 // Importar dependencias
-import 'cypress-file-upload'; // Para carga de archivos
-require('cypress-xpath'); // Para soporte de xpath
-require('cypress-plugin-tab'); // Para simular teclas de tabulación
-require('@4tw/cypress-drag-drop'); // Para simular arrastrar y soltar
 
 describe('Título', () => {
     it('Test', () => {
